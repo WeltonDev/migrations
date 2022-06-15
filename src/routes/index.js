9754b919cd8f19093e250e5e15f6491976ee39d8
WeltonDev <welton.developer95@gmail.com>
2022-06-14 22:17:20 -0300

    Validar dados OK || Autenticar dados ok ||

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,33 @@
 const express = require("express");
+
+// Controllers
 const postsController = require("../controllers/posts/postsController");
 const usersController = require("../controllers/users/usersController");
+const authController = require("../controllers/auth/authController");
+
+// Validação de dados nas requisições
+const userCreateValidate = require("../validations/users/create");
+const postCreateValidate = require("../validations/posts/create");
+const loginValidate = require("../validations/auth/login");
+
+// Autenticação de dados
+const auth = require("../middleware/auth");
 
 const routes = express.Router();
 
-routes.get("/", usersController.listUsers);
-routes.post("/create", usersController.createUser);
-routes.put("/update/:id", usersController.updateUser);
-routes.delete("/delete/:id", usersController.deleteUser);
+// Login
+routes.post("/login", loginValidate, authController.login);
 
-routes.get("/posts", postsController.listPosts);
-routes.post("/posts", postsController.createPost);
-routes.put("/posts/:id", postsController.updatePost);
-routes.delete("/posts/:id", postsController.deletePost);
+// Usuarios
+routes.get("/", auth, usersController.listUsers);
+routes.post("/create", userCreateValidate, usersController.createUser);
+routes.put("/update/:id", auth, usersController.updateUser);
+routes.delete("/delete/:id", auth, usersController.deleteUser);
 
+// Posts
+routes.get("/posts", auth, postsController.listPosts);
+routes.post("/posts", auth, postsController.createPost);
+routes.put("/posts/:id", auth, postsController.updatePost);
+routes.delete("/posts/:id", auth, postsController.deletePost);
 
 module.exports = routes;
